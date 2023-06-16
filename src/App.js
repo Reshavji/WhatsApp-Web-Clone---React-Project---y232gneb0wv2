f83e9be29258b5f439fe09d429fc04f21ba83128
Reshav ji <116823969+Reshavji@users.noreply.github.com>
2023-06-16 22:43:19 +0530

    Update

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import Home from './Components/Home';
+import './App.css';
+
+function App() {
+  return (
+    <>
+    <Home></Home>
+    </>
+  );
+}
+
+export default App;
---
+++ src/Components/Home.js
@@ -0,0 +1,11 @@
+ const Home =()=>{
+    return(
+    <div id="container">
+            <div id="box">
+                 <img id="logo" src="https://static.vecteezy.com/system/resources/previews/018/930/746/non_2x/whatsapp-logo-whatsapp-icon-whatsapp-transparent-free-png.png" alt="whatapp"/>
+           <button id="signinbtn">Sign in with google</button>
+            </div>
+    </div>
+    )
+}
+export default Home;
