a01b476ad4a05a61ce45b0b726a955d02c01037c
Reshav ji <116823969+Reshavji@users.noreply.github.com>
2023-06-19 12:20:17 +0530

    Update

--- src/Components/ChatBox.js
+++ src/Components/ChatBox.js
@@ -1,8 +1,9 @@
-import React, { useState } from 'react';
+import React, { useState, useRef, useEffect } from 'react';
 
 const ChatBox = () => {
   const [message, setMessage] = useState('');
   const [chatHistory, setChatHistory] = useState([]);
+  const chatContainerRef = useRef(null);
 
   const handleInputChange = (e) => {
     setMessage(e.target.value);
@@ -15,22 +16,38 @@ const ChatBox = () => {
     }
   };
 
+  const handleKeyDown = (e) => {
+    if (e.key === 'Enter') {
+      handleSendMessage();
+    }
+  };
+
+  useEffect(() => {
+    // Scroll to the bottom of the chat container whenever chatHistory changes
+    if (chatContainerRef.current) {
+      chatContainerRef.current.scrollTop = chatContainerRef.current.scrollHeight;
+    }
+  }, [chatHistory]);
+
   return (
     <>
-    <div className='chat-container'>
-      <div className="chat-history">
-        {chatHistory.map((msg, index) => (
-          <React.Fragment key={index}>
-            <div className='message-container'>
-            <span className="message">{msg}</span></div>
-          </React.Fragment>
-        ))}
-      </div></div>
+      <div className='chat-container' ref={chatContainerRef}>
+        <div className="chat-history">
+          {chatHistory.map((msg, index) => (
+            <React.Fragment key={index}>
+              <div className='message-container'>
+                <span className="message">{msg}</span>
+              </div>
+            </React.Fragment>
+          ))}
+        </div>
+      </div>
       <div className="input-container" id="inputContainer">
         <input
           type="text"
           value={message}
           onChange={handleInputChange}
+          onKeyDown={handleKeyDown} // Add the onKeyDown event listener
           placeholder="Type your message..."
         />
         <button onClick={handleSendMessage}>Send</button>
