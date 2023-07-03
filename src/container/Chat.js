1fa5078d1cf85616b7dde7e979ae05aaf97679c3
Reshav ji <116823969+Reshavji@users.noreply.github.com>
2023-07-03 18:30:52 +0530

    Update Chat.js

--- src/container/Chat.js
+++ src/container/Chat.js
@@ -71,7 +71,7 @@ function Chat() {
         <Avatar src={messages[messages.length - 1]?.data?.photoURL} />
         <div className="chat__headerInfo">
           <h3>{roomName}</h3>
-          <p>{messages[0] && timeFromNow(messages[messages.length - 1]?.data?.timestamp)}</p>
+          <p>online</p>
         </div>
         <div className="chat__headerIcons">
           <IconButton>
